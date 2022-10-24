789105f8265c88666102c1ff70ec8575fc1e2f5a
ritukanwer <ritunarukarajput.21@gmail.com>
2022-10-24 23:14:50 +0530

    function calling

--- day_1.py
+++ day_1.py
@@ -1,4 +1,19 @@
-from day_2 import find_even_numbers
+from day_2 import find_even_numbers, length_of_dictionary
 lst = [1,2,3,5,6,8,9,10,4]
 x= find_even_numbers(lst)
 print(x)
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+}
+
+y = length_of_dictionary(ages)
+print("the length of dictionary:- ",y)
