6438e2a5dfe0ae268e44d6d90713c58980041eef
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-07 14:24:13 +0530

    question_17

---
+++ question_17.py
@@ -0,0 +1,9 @@
+# question_17
+# Given the code below, use string formatting with the format() method on line 3 to map the values of 2010, 10k and Bitcoin to the corresponding formatting operators.
+
+
+
+
+my_string = "In {}, someone paid {} {} for two pizzas."
+
+print(my_string.format("2010","10k","Bitcoin"))
