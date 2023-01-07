6683c75eba5cd2eaf6a33a1bc502a975b9b6fd3a
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-07 13:59:58 +0530

    question_8

---
+++ Question_8.py
@@ -0,0 +1,10 @@
+
+
+# question_8
+
+# Given the code below, insert the correct method on line 3 in order to check of the string starts with the letter X
+
+
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(my_string.startswith("X"))
