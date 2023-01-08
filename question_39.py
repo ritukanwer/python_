291801949d022c25584e939c57daaeff0efa1cf6
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-08 22:50:36 +0530

    question_39

---
+++ question_39.py
@@ -0,0 +1,5 @@
+
+# Given the code below, use the correct method on line 3 in order to remove the element 30 from my_list.
+my_list = [10,10.5,20,30,'python','java','ruby']
+my_list.pop(3)
+print(my_list)
