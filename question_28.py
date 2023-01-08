7fc9dcc4b2bf4afd329b1ddc58df00ecdfe9972b
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-08 22:26:40 +0530

    question_28

---
+++ question_28.py
@@ -0,0 +1,9 @@
+
+
+# Given the code below, use the correct math operator in between num1 and num2 on line 4 in order to obtain the result shown.
+num_1 = 25
+num_2 = 8
+num_3 = num_1 % num_2
+print(num_3)
+
+
