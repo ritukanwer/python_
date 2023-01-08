8e4097d3a34a8f5df0a22bed46003ffe9ec60544
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-08 22:28:05 +0530

    question_29

---
+++ question_29.py
@@ -0,0 +1,9 @@
+
+# Given the code below, use the correct math operator in between num1 and num2 on line 4 in order to obtain the result shown
+
+
+num_1 = 10
+num_2 = 3
+
+num_3 = num_1 ** num_2
+print(num_3 == 250*4)
