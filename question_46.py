fc45eadeabc55bf175308dddd6d807ffd2a1877b
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-09 21:42:27 +0530

    question_46

---
+++ question_46.py
@@ -0,0 +1,8 @@
+# Given the code below, use the correct function on line 3 in order to find out the smallest number in my_list.
+
+
+
+my_list = [10, 10.5, 20, 30, 25.6, 19.25, 11.01, 29.99]
+
+a = min(my_list)
+print(a)
