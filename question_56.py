5cb09a10d49025e7f59d71ae2c586d4c6b212f94
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-10 22:18:02 +0530

    question_56

---
+++ question_56.py
@@ -0,0 +1,7 @@
+# Given the code below, use the correct code on line 3 in order to return my_list except the last 4 elements,
+# using a single, negative index
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+
+my_slice = my_list[:-4]
+
+print(my_slice)
