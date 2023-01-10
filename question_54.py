93c28fb5adf2795bcee077a49f91ed3d3beb8ddf
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-10 22:16:11 +0530

    question_54

---
+++ question_54.py
@@ -0,0 +1,10 @@
+
+# Given the code below, use the correct code on line 3 in order to return a slice made of [30, 'Python', 'Java'] from
+# my_list based on negative index
+
+
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+
+my_slice = my_list[-4:-1]
+
+print(my_slice)
