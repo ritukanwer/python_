1c926516cf26242302687f1b15b1e2df5f5f2336
ritukanwer <ritunarukarajput.21@gmail.com>
2023-01-10 22:17:06 +0530

    question_55

---
+++ question_55.py
@@ -0,0 +1,7 @@
+# Given the code below, use the correct code on line 3 in order to return my_list except the first 3 elements,
+# using a single, positive index
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+
+my_slice = my_list[3:]
+
+print(my_slice)
