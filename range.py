1f4b966181f14b37bf2ab76ea6f7c8317cd18cf8
ritukanwer <ritunarukarajput.21@gmail.com>
2023-02-20 15:24:07 +0530

    range

---
+++ range.py
@@ -0,0 +1,68 @@
+
+# question_78
+# Given the code below, use the correct argument(s) for the range() function on line 1 in order to return a range of consecutive
+# integers from 0 to 9 inclusively use a single argument inside the parentheses of range()!
+
+my_range = range(10)
+
+print(list(my_range))
+
+
+
+# question_79
+
+# Given the code below, use the correct argument(s) for the range() function on line 1 in order to return a range of consecutive
+# integers from 0 to 9 inclusively use two argument inside the parentheses of range()!
+
+
+my_range = range(0, 10)
+
+print(list(my_range))
+
+
+# question_80
+
+# correct argument(s) for the range() function on line 1 in order to return a range of consecutive integers from 117 to 129 exclusively.
+my_range = range(117,129)
+
+print(list(my_range))
+
+
+
+# question_81
+# Given the code below, use the correct argument(s) for the range() function on line 1 in order to return [10, 13, 16, 19] when converted to a list.
+my_range = range(10,20,3)
+
+print(list(my_range))
+
+
+
+# question_82
+# Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [115, 120] when converted to a list.
+my_range = range(115,125,5)
+
+print(list(my_range))
+
+
+
+
+
+# question_83
+# Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [-75, -60, -45, -30] when converted to a list.
+my_range = range(-75,-25,15)
+print(list(my_range))
+
+
+# question_84
+# Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [-75, -60, -45, -30] when converted to a list.
+
+my_range = range(-25,139,30)
+print(list(my_range))
+
+
+
+# question_85
+# Given the code below, write the correct range on line 1 in order to return [-10] when converted to a list.
+
+my_range = range(-10,-9)
+print(list(my_range))
