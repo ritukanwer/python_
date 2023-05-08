ca09b0a8936219955d4312de7222c717d0499e1a
ritukanwer <ritunarukarajput.21@gmail.com>
2023-05-08 22:23:41 +0530

    question

--- test_question.py
+++ test_question.py
@@ -21,18 +21,22 @@ input_list = [{'name': 'Alice', 'age': 25, 'favorite_foods': ['pizza', 'tacos', 
  {'name': 'David', 'age': 40, 'favorite_foods': ['steak', 'lobster', 'chocolate']}]
 
 
-
+"""i am create a function name is name_and_count_fav_food. then i a take a peramiter input_list"""
 def name_and_count_fav_foods(input_list):
+   #  then i take a variable answer name and create a empty list
    answer = []
+   # then i started for loop in input_list. the loop is read one by one read all input_list items.
    for i in input_list:
-
+        # then i take a variable and write name = i['name'] access all name.
       name = i['name']
+        # then i took len of all number of fav_foods.
       num_favorite_foods = len(i['favorite_foods'])
+        # then i append answer name of empty list and add name and favorate foods .
       answer.append({'name':name,'num_favorite_foods':num_favorite_foods})
 
-
+# then i return the answer
    return answer
-# print(input_list)
+# then i take a  x name variable and print THIS
 x = name_and_count_fav_foods(input_list)
 print(x)
 
