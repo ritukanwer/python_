3c075cd9cc74fa03f065a1280009e42ea6436d20
ritukanwer <ritunarukarajput.21@gmail.com>
2023-10-31 19:50:53 +0530

    git commits

---
+++ git_commits.py
@@ -0,0 +1,28 @@
+
+def get_repos_names(user_name):
+    access_token = "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"
+    repository = "python_"
+    url = f"https://api.github.com/repos/{user_name}/{repository}/commits"
+    headers = {
+        'Authorization': f'token {access_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+    my_data = response.json()
+    # commit_list = []
+
+    for commit in my_data:
+        print(commit)
+    #     commit_dict = {
+    #         "sha": commit['sha'],
+    #         "author_name":commit['commit']['author']['name'],
+    #         "date":commit['commit']['author']['date']
+    #     }
+    #     commit_list.append(commit_dict)
+    # return commit_list
+
+
+print(get_repos_names(user_name = 'ritukanwer'))
+
+
+
