cfcb5995a430a26edf536d4ad5873e70c9be726b
ritukanwer <ritunarukarajput.21@gmail.com>
2023-10-31 19:56:06 +0530

    git_api

---
+++ github_api.py
@@ -0,0 +1,24 @@
+
+
+import requests
+def find_repo_name(username):
+    access_token = "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"
+    url = f"https://api.github.com/users/{username}/repos"
+    headers = {'authorization' : f'tokan{access_token}'
+    }
+    response = requests.get(url, headers=headers)
+    my_data = response.json()
+    print(my_data)
+    repository_name = []
+    for commit in my_data:
+        repos_name = commit['name']
+        repository_name.append(repos_name)
+
+    return repository_name
+
+print(find_repo_name(username = 'ritukanwer'))
+
+
+
+
+
