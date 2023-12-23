13267e59b194034db67ec6b3216cfff4dac4b536
ritukanwer <ritunarukarajput.21@gmail.com>
2023-12-23 20:25:52 +0530

    course_exe

--- course_exe.py
+++ course_exe.py
@@ -7,43 +7,12 @@ You are old enough to learn to drive.
 Output:
 Enter your age: 15
 You need 3 more years to learn to drive."""
-# age_str = input("Enter your age: ")
-#
-# age = int(age_str)
-#
-# if age >= 18:
-#     print("You are old enough to learn to drive.")
-# else:
-#     years_to_wait = 18 - age
-#     print(f"You need {years_to_wait} more years to learn to drive.")
+age_str = input("Enter your age: ")
 
-"""
-Compare the values of my_age and your_age using if … else. Who is older (me or you)?
-Use input(“Enter your age: ”) to get the age as input. You can use a nested condition
-to print 'year' for 1 year difference in age, 'years' for bigger differences, and a
-custom text if my_age = your_age. Output:
-Enter your age: 30
-You are 5 years older than me."""
+age = int(age_str)
 
-
-
-# your_age = input("enter your age :")
-# my_age = 18
-# if your_age > my_age:
-#     diff = your_age - my_age
-#     print(f"you are {diff}year old then me")
-# elif your_age < my_age:
-#     print(f"I am {diff} year older than you.")
-# else:
-#     print("we are same age")
-
-
-Get two numbers from the user using input prompt.
-If a is greater than b return a is greater than b, if a is less b
-return a is smaller than b, else a is equal to b. Output:
-
-Enter number one: 4
-Enter number two: 3
-4 is greater than 3
-
-
+if age >= 18:
+    print("You are old enough to learn to drive.")
+else:
+    years_to_wait = 18 - age
+    print(f"You need {years_to_wait} more years to learn to drive.")
