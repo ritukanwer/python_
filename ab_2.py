d436e9b02b96936a4ca3a474043f4245ccc8e679
ritukanwer <ritunarukarajput.21@gmail.com>
2024-01-08 19:39:43 +0530

    practice_exercise

--- ab_2.py
+++ ab_2.py
@@ -129,15 +129,3 @@ for i in range(3):
         print()
     print()
 
-"""
-#Write a Python program to count the number of unique sublists within a given list.
-Original list:
-[[1, 3], [5, 7], [1, 3], [13, 15, 17], [5, 7], [9, 11]]
-Number of unique lists of the said list:
-{(1, 3): 2, (5, 7): 2, (13, 15, 17): 1, (9, 11): 1}
-Original list:
-[['green', 'orange'], ['black'], ['green', 'orange'], ['white']]
-Number of unique lists of the said list:
-{('green', 'orange'): 2, ('black',): 1, ('white',): 1} """
-
-list = [[1, 3], [5, 7], [1, 3], [13, 15, 17], [5, 7], [9, 11]]
